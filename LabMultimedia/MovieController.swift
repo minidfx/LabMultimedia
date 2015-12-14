c9dd62ff36437a93ef89ae0fd9a9e51ecb257551
Burgy Benjamin <minidfx@gmail.com>
2015-12-14 10:58:39 +0100

    First version using segue and the default AVPlayer view.

---
+++ LabMultimedia/MovieController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieButtonController.swift
+//  LabMultimedia
+//
+//  Created by Benjamin Burgy on 14.12.15.
+//  Copyright © 2015 HES-SO Master. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieController: UIViewController {
+    
+}
+
---
+++ LabMultimedia/MoviePlayerController.swift
@@ -0,0 +1,14 @@
+//
+//  MoviePlayerController.swift
+//  LabMultimedia
+//
+//  Created by Benjamin Burgy on 14.12.15.
+//  Copyright © 2015 HES-SO Master. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+class MoviePlayerController: AVPlayerViewController {
+    
+}
