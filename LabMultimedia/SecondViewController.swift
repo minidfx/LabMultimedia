6efd91015028bee2cda5179706bc5ab82bfd9f6a
Burgy Benjamin <minidfx@gmail.com>
2015-12-14 09:33:08 +0100

    Initial Commit

---
+++ LabMultimedia/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LabMultimedia
+//
+//  Created by Benjamin Burgy on 14.12.15.
+//  Copyright © 2015 HES-SO Master. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
